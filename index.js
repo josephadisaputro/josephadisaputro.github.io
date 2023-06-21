620a9adf76346bd19324e33c929400fe0029308e
Yusuf Adisaputro <yusuf.adisaputro@outlook.com>
2023-06-21 20:56:48 +0700

    20230621_First_Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs');
+const app = express()
+
+app.use(cors())
+
+app.get('/', async function (req, res, next) {
+  res.sendFile(__dirname + `/views/index.html`)
+})
+
+app.get('/api/:path', async function (req, res, next) {
+    const data = (await readFile(`./data/${req.params.path}.json`, true))
+    let response = {}
+    if(data.status){
+        response = data.data
+    }
+    res.json(response)
+})
+
+app.get('/ip', async function (req, res, next) {
+    res.json({
+        ip: req.socket.remoteAddress
+    })
+  })
+
+async function readFile(filename, isJSON = false){
+    return new Promise(async (resolve, reject) => {
+        await fs.readFile(`${filename}`, async (err, data) => {
+            if (err){
+                console.log(err)
+                resolve({
+                    status: false,
+                    data: []
+                })
+            }
+            if(isJSON){
+                data = JSON.parse(data);
+            }
+            resolve({
+                status: true,
+                data
+            })
+        });
+    });
+}
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
